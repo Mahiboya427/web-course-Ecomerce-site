a0d288fdf1de338f395422c8684301c8267507d3
Boya Mahesh <63939597+Mahiboya427@users.noreply.github.com>
2023-05-26 16:43:43 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,65 @@
+
+            
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+    import { getAuth,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+   
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyCxf3ikoJWkzHtvUCBoTnNjT1mRg8e4NzU",
+      authDomain: "auth-courseofferings.firebaseapp.com",
+      projectId: "auth-courseofferings",
+      storageBucket: "auth-courseofferings.appspot.com",
+      messagingSenderId: "484839394488",
+      appId: "1:484839394488:web:b9d2f93946b5f945eab84d",
+      measurementId: "G-PMQ4DGFSBZ"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const auth = getAuth();
+
+    
+//var username=document.getElementById("username");
+var login_email=document.getElementById("login-email");
+//var register_email=document.getElementById("register-email");
+var login_password=document.getElementById("login-password");
+//var register_password=document.getElementById("register-password");
+
+
+console.log(login_email.value,login_password.value);
+
+
+window.login=function (e) {
+    e.preventDefault();
+    var obj1={
+        email:login_email.value,
+        password:login_password.value
+    };
+    console.log(obj1);
+
+
+    signInWithEmailAndPassword(auth,obj1.email,obj1.password)
+        .then(function(success) {
+            console.log(user.uid);
+            alert("login sucessfull")   
+        
+    })
+        .catch(function (err) {
+        alert("login error"+err)
+                
+    })
+    console.log(obj1);
+
+};
+
+
+
+
+     
