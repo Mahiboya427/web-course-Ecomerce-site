a267d4f6911b4a3dde91d2238748db966694f3e5
Boya Mahesh <63939597+Mahiboya427@users.noreply.github.com>
2023-05-30 15:47:20 +0530

    Add files via upload

---
+++ element.js
@@ -0,0 +1,13 @@
+var element_title_1=document.getElementById("element-title-1").textContent;
+var element_category_1=document.getElementById("element-category-1").textContent;
+var element_heading_1=document.getElementById("element-heading-1").textContent;
+var element_rating_1=document.getElementById("element-rating-1").textContent;
+var element_price_1=document.getElementById("element-price-1").textContent;
+var element_duration_1=document.getElementById("element-duration-1").textContent;
+
+console.log("ekement-title:",element_title_1,";",
+"element_category:",element_category_1,";",
+"element_heading:",element_heading_1,";" ,
+"element_rating:",element_rating_1,";",
+"element_price:",element_price_1,";",
+"element_duration:",element_duration_1);
