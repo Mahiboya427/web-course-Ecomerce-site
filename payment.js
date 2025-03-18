50535b641da90cc4b26a516aef7562d36ca3e175
Boya Mahesh <boyamahesh568@gmail.com>
2025-03-18 12:49:17 +0530

    PAYMENT8

--- payment.js
+++ payment.js
@@ -150,7 +150,7 @@ document.addEventListener("DOMContentLoaded", () => {
         postData();
 
 
-        window.location.assign('./final.html');
+        //window.location.assign('./final.html');
 
     };
 
